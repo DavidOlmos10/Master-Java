bf3c529df62661ae8c774dc5e35bef9e68879094
David Antonio Olmos Ruiz <jamesdavid9810@outlook.com>
2023-02-21 13:47:57 -0600

    Tipos de Variables
    Empezando y conociendo los tipos de variables

---
+++ Ejercicio-02-Variables/src/Variables.java
@@ -0,0 +1,30 @@
+
+public class Variables {
+    
+    public static void main(String args[]){
+        //Definimos la Variable 
+         int miVariableEntera = 10;
+        
+        System.out.println(miVariableEntera);
+        
+       //Modificamos el valor de la variable
+        miVariableEntera = 5;
+        System.out.println (miVariableEntera);
+        
+        String miVariableCadena = "Saludos";
+        System.out.println(miVariableCadena);
+        
+        miVariableCadena = "Adios";
+        System.out.println(miVariableCadena);
+        
+        //var  - Inferencia de tipos en Java
+        
+        var miVariableEntera2 = 98;
+        System.out.println(miVariableEntera2);
+        
+        var miVariableCadena2 = "Goodbye";
+        System.out.println(miVariableCadena2);
+        
+    }
+}
+ 
