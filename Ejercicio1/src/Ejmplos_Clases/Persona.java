5c8989a188e7d6ad302e3a1c25c293b2bde98cf4
David Antonio Olmos Ruiz <jamesdavid9810@outlook.com>
2023-07-17 11:03:04 -0600

    Ejemplos de clases
    Se crearon ejemplos con objetos, metodos e instancias

---
+++ Ejercicio1/src/Ejmplos_Clases/Persona.java
@@ -0,0 +1,16 @@
+
+package Ejmplos_Clases;
+
+
+public class Persona {
+    //Definimos los atributos de la clase
+    String nombre;
+    String apellido;
+    
+    public void despelgarInformacion(){
+        System.out.println("Nombre:" + nombre);
+        System.out.println("Apellido:" + apellido);
+    }
+    
+    
+}
